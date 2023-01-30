3c07c85bd8ebede51d59e2729d535df6f1a0f847
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-30 16:38:13 -0600

    set up service and context for signing in

---
+++ src/context/AuthContext.js
@@ -0,0 +1,22 @@
+import { useState, useContext } from 'react';
+import { createContext } from 'react';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+const useUser = () => {
+  const data = useContext(UserContext);
+
+  if (!data) {
+    throw new Error('I see you are trying to useUser, where is your buddy UserProvider?');
+  }
+  return data;
+};
+
+export { UserProvider, useUser };
